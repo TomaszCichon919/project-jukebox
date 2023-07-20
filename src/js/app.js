798965d4e13eef93bd0a59d872f21ef7945e9de8
Tomasz Cichon <tomekcichon@tlen.pl>
2023-07-20 19:20:16 +0200

    add base files in progress

---
+++ src/js/app.js
@@ -0,0 +1,88 @@
+
+const app = {
+    initPages: function() {
+      const thisApp = this;
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+
+      thisApp.navLinks = document.querySelectorAll('.main-nav a, .box-link');
+
+      const idFromHash = window.location.hash.replace('#/', '');
+      
+
+      let pageMatchingHash = thisApp.pages[2].id;
+
+      for(let page of thisApp.pages) {
+        if(page.id == idFromHash){
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+      thisApp.activatePage(pageMatchingHash);
+      for(let link of thisApp.navLinks) {
+        link.addEventListener('click', function(event){
+          const clickedElement = this;
+          event.preventDefault();
+
+          /* get pagfe id from href */
+          const id = clickedElement.getAttribute('href').replace('#', '');
+          /* run thisApp.activatePAge with that id */
+          thisApp.activatePage(id);
+
+          /* change url hash*/
+          window.location.hash = '#/' + id;
+        });
+      }
+      
+    },
+
+    activatePage: function (pageId){
+      const thisApp = this;
+      /* add class active to maching pages, remove from non-matching */
+      for(let page of thisApp.pages) {
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
+      /* add class active to maching links, remove from non-matching */
+      for(let link of thisApp.navLinks) {
+        link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+      }
+
+    },
+
+initData: function () {
+    const thisApp = this;
+
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
+    fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+       .then(function(parsedResponse){
+       // console.log('parsedResponse', parsedResponse);
+
+        /* save parsedResponse as thisApp.data.products */
+        thisApp.data.products = parsedResponse;
+
+        /* execute initMenu method */
+        thisApp.initMenu();
+
+       });
+       // console.log("thisApp.data", JSON.stringify(thisApp.data));
+  },
+
+init: function () {
+    const thisApp = this;
+    // console.log('*** App starting ***');
+    // console.log('thisApp:', thisApp);
+    // console.log('classNames:', classNames);
+    // console.log('settings:', settings);
+    // console.log('templates:', templates);
+    thisApp.initData ();
+    thisApp.initPages();
+},
+
+};
+
+
+app.init();
